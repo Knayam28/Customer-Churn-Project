5e1532270bf6683583d84f766b651db84f6cf3f4
Knayam28 <mayanksingh82001@gmail.com>
2026-03-11 13:46:07 +0530

    Update xgb_app.py

--- CustChurnProj/xgb_app.py
+++ CustChurnProj/xgb_app.py
@@ -17,7 +17,7 @@ st.markdown("""
 # 2. Load Model
 @st.cache_resource
 def load_model():
-    return joblib.load('Model/xgb_churn_pipeline.pkl')
+    return joblib.load('xgb_churn_pipeline.pkl')
 
 try:
     model = load_model()
@@ -130,6 +130,7 @@ if predict_clicked:
     except Exception as e:
 
         st.error(f"An error occurred during prediction: {e}")
+
 
 
 
